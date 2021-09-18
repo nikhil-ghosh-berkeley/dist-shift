f2bd3596bd29db84b20c2623ef9bb1330cbec4bc
Nikhil Ghosh <nikhil_ghosh@shelob.Berkeley.EDU>
2021-09-17 19:24:06 -0700

    added requirements.txt and changed opt_df.py to use cats

--- opt_df.py
+++ opt_df.py
@@ -6,8 +6,7 @@ from src.utils import load_pickle
 osj = os.path.join
 preds_save_loc = "predictions_early_stopping/"
 
-exp_config = load_pickle(osj(preds_save_loc, "exp_config.pkl"))
-hash_dict = exp_config["hash_dict"]
+cats = load_pickle(osj(preds_save_loc, "cats.pkl"))
 
 print("start reading")
 read_start = time.time()
@@ -23,9 +22,8 @@ df = pd.read_csv(
     },
 )
 
-col_list = ["hash", "name", "seed"]
-for col in col_list:
-    df[col] = df[col].cat.rename_categories(lambda x: exp_config[col].obj_list[int(x)])
+for col in cats:
+    df[col] = df[col].cat.rename_categories(lambda x: cats[col].obj_list[int(x)])
 
 print("reading finished: elapsed %f" % (time.time() - read_start))
 pd.to_pickle(df, osj(preds_save_loc, "preds.pkl"))
